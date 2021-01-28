f553d1f8e9f5ebe1b9c2560bf9fbaaf7b7f4c322
michlSz <54441422+michlSz@users.noreply.github.com>
2021-01-28 12:02:22 +0100

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
+define("DB_HOST", "");
+define("DB_USER", "");
 define("DB_PASS", "");
-define("DB_NAME", "cms3");
+define("DB_NAME", "");
 
 
 $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
